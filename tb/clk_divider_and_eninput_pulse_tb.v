78f85ed1412e3d2a75126c9a5925b955f1027ad2
truong92cdv <truong92cdv@gmail.com>
2025-01-26 08:28:43 +0700

    Update clk_divider_and_eninput_pulse_tb.v

--- tb/clk_divider_and_eninput_pulse_tb.v
+++ tb/clk_divider_and_eninput_pulse_tb.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+
+module clk_divider_and_eninput_pulse_tb();
+    reg     clk;
+    wire    clk_10Hz;
+    wire    en_input;
+
+    clk_divider #(
+        .input_clk_freq (100_000_000),
+        .output_clk_freq(100)
+    ) uut (
+        .clk            (clk),
+        .clk_10Hz       (clk_10Hz)
+    );
+
+    gen_eninput_pulse #(
+        .input_clk_freq (100_000_000),
+        .output_clk_freq(100)
+    ) uut2 (
+        .clk            (clk),
+        .clk_10Hz       (clk_10Hz),
+        .en_input       (en_input)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+endmodule
 
